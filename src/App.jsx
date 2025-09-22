0db0a0d2bf50af0a0c23236ae0249915f117764c
aeliyadevs <aeliyadevs@gmail.com>
2025-09-22 08:54:22 +0545

    refactor: replace BrowserRouter with HashRouter for static hosting on gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import AboutSection from "./components/AboutSection";
-import ContactSection from "./components/ContactSection";
-import EducationSection from "./components/EducationSection";
-import ExperiencesSection from "./components/ExperiencesSection";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import ProjectsSection from "./components/ProjectsSection";
-import ServicesSection from "./components/ServicesSection";
-import SkillsSection from "./components/SkillsSection";
 import Projects from "./pages/Projects";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/:id" element={<Details />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
